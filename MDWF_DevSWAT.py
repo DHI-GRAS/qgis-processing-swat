41d70f54d98ff11a306538b33b5892f2f3299a36
Rado Guzinski <rmgu@dhi-gras.com>
2014-05-22 13:23:03 +0200

    Avoid problems with hanging subprocesses.

--- MDWF_DevSWAT.py
+++ MDWF_DevSWAT.py
@@ -26,8 +26,7 @@ class MDWF_DevSWAT(GeoAlgorithm):
 
         #MapWindow_path = 'C:\Program Files (x86)\MapWindow\MapWindow.exe'
         MapWindow_path = os.path.join(WG9HMUtils.mapwindowPath(),'MapWindow.exe')
-        subprocess.call([MapWindow_path])
-
+        subprocess.Popen([MapWindow_path])
 
     def getIcon(self):
         return  QtGui.QIcon(os.path.dirname(__file__) + "/images/tigerNET.png")
